d9c631ffdc410672b8bbdb1efd351d335a6cfd7b
Sworno Das <95184683+SwornoDas@users.noreply.github.com>
2025-07-31 17:51:07 +0530

    Added Question 2

---
+++ Day 1/q2.py
@@ -0,0 +1,19 @@
+# Program to perform addition, subtraction, multiplication, division
+# integer division and modulo division on two integer numbers
+
+# Input two integer numbers from the user
+a = int(input("Enter first number. "))
+b = int(input("Enter second number. "))
+
+# Perform and display addition
+print("Addition :", a+b)
+# Perform and display subtraction
+print("Subtraction :", a-b)
+# Perform and display multiplication
+print("Multiplication :", a*b)
+# Perform and display division (floating point)
+print("Division :", a/b)
+# Perform and display integer division (quotient)
+print("Integer Division :", a//b)
+# Perform and display modulo division (remainder)
+print("Modulo :", a%b)
